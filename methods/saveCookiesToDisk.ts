b18a2c669452c51b877dadffa09df3489cb8218d
gladiatortoise <ezhacks2014@gmail.com>
2021-02-01 07:45:48 +0200

    Make cookies missing throw an error
    I'm not quite sure why cookies missing should not throw an error.

--- methods/saveCookiesToDisk.ts
+++ methods/saveCookiesToDisk.ts
@@ -1,6 +1,6 @@
 import fs from 'fs-extra'
 import {IWebDriverCookie} from 'selenium-webdriver'
 
-export default async (cookies: IWebDriverCookie[] = [], path: string): Promise<void> => {
+export default async (cookies: IWebDriverCookie[], path: string): Promise<void> => {
     return fs.writeJSON(path, cookies)
 }
